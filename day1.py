aec4e23634887b1c79045213f261445966ced5c8
CRadley <61577912+CRadley@users.noreply.github.com>
2022-12-01 08:53:15 +0000

    Day 1

--- day1.py
+++ day1.py
@@ -1,16 +1,42 @@
-from functools import reduce
+"""
+Advent of Code: Day 1
+"""
 from typing import List
 
-with open("resources/day1.txt") as f:
-    lines = [line.strip() for line in f.readlines()]
+def read_input_file(file_path: str) -> str:
+    """
+    Reads and returns input file content
+    """
+    with open(file_path, encoding="utf-8") as input_file:
+        return input_file.read()
 
-def elf_reducer(elves: List[int], line: str) -> List[int]:
-    if line.isdigit():
-        elves[-1] += int(line)
-    else:
-        elves.append(0)
-    return elves
 
-elves = reduce(elf_reducer, lines, [0])
-print(max(elves))
-print(sum(sorted(elves, reverse=True)[0:3]))
+def determine_elves(file_input: str) -> List[int]:
+    """
+    Determines the calorie count for each elf
+    """
+    return [sum(map(int, elf.split())) for elf in file_input.split("\n\n")]
+
+def determine_largest_calories(elves: List[int]) -> int:
+    """
+    Returns the largest calorie value
+    """
+    return max(elves)
+
+def determine_largest_calorie_sum(elves: List[int]) -> int:
+    """
+    Determines the sum of the three largest calorie values
+    """
+    return sum(sorted(elves, reverse=True)[0:3])
+
+def main() -> None:
+    """
+    Main function
+    """
+    file_input = read_input_file("resources/day1.txt")
+    elves = determine_elves(file_input)
+    print(determine_largest_calories(elves))
+    print(determine_largest_calorie_sum(elves))
+
+if __name__ == "__main__":
+    main()
