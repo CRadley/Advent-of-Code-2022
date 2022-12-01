b1c356b8dd365aa814ebae2d239e0d12fd66bc40
CRadley <61577912+CRadley@users.noreply.github.com>
2022-12-01 06:59:05 +0000

    Day 1: Rename values to lines, value to line

--- day1.py
+++ day1.py
@@ -2,15 +2,15 @@ from functools import reduce
 from typing import List
 
 with open("resources/day1.txt") as f:
-    values = [line.strip() for line in f.readlines()]
+    lines = [line.strip() for line in f.readlines()]
 
-def elf_reducer(elves: List[int], value: str) -> List[int]:
-    if value.isdigit():
-        elves[-1] += int(value)
+def elf_reducer(elves: List[int], line: str) -> List[int]:
+    if line.isdigit():
+        elves[-1] += int(line)
     else:
         elves.append(0)
     return elves
 
-elves = reduce(elf_reducer, values, [0])
+elves = reduce(elf_reducer, lines, [0])
 print(max(elves))
 print(sum(sorted(elves, reverse=True)[0:3]))
