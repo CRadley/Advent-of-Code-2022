980fdb4da23919258f278a3c4fd9433521d472ea
CRadley <61577912+CRadley@users.noreply.github.com>
2022-12-01 08:57:06 +0000

    Add black

--- day1.py
+++ day1.py
@@ -3,6 +3,7 @@ Advent of Code: Day 1
 """
 from typing import List
 
+
 def read_input_file(file_path: str) -> str:
     """
     Reads and returns input file content
@@ -17,17 +18,20 @@ def determine_elves(file_input: str) -> List[int]:
     """
     return [sum(map(int, elf.split())) for elf in file_input.split("\n\n")]
 
+
 def determine_largest_calories(elves: List[int]) -> int:
     """
     Returns the largest calorie value
     """
     return max(elves)
 
+
 def determine_largest_calorie_sum(elves: List[int]) -> int:
     """
     Determines the sum of the three largest calorie values
     """
     return sum(sorted(elves, reverse=True)[0:3])
+
 
 def main() -> None:
     """
@@ -37,6 +41,7 @@ def main() -> None:
     elves = determine_elves(file_input)
     print(determine_largest_calories(elves))
     print(determine_largest_calorie_sum(elves))
+
 
 if __name__ == "__main__":
     main()
