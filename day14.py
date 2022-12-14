1c865b7293f33785d70c0aeefeba32e920c95506
Connor Radley <connor.radley@hotmail.co.uk>
2022-12-14 18:48:34 +0000

    Day 14 fixed :)

--- day14.py
+++ day14.py
@@ -5,6 +5,7 @@ def read_input_file(file_path) -> str:
     with open(file_path, encoding="utf-8") as input_file:
         return input_file.read()
 
+
 def determine_positions(x, y, x1, y1):
     if x == x1:
         modifier = 1 if y < y1 else -1
@@ -14,72 +15,71 @@ def determine_positions(x, y, x1, y1):
     x_positions = range(x, x1 + modifier, modifier)
     return list(zip(x_positions, [y] * len(x_positions)))
 
+
 def convert_rock_position(rock):
     return list(map(int, rock.split(",")))
 
+
 def determine_sub_invalid_rocks(rock_positions):
     rocks = []
-    for i in range(1,(len(rock_positions)//2 + 1)):
+    for i in range(1, (len(rock_positions)//2 + 1)):
         new_positions = [(r[0], r[1] + i + 1) for r in rock_positions[i:-i]]
         rocks.extend(new_positions)
     return rocks
 
-def determine_rocks_and_air(file_input):
+
+def determine_rocks(file_input):
     valid_rocks = []
-    air = []
     for line in file_input.splitlines():
-        matches = list(map(convert_rock_position,re.findall(r"[\d]+,[\d]+", line)))
+        matches = list(map(convert_rock_position,
+                           re.findall(r"[\d]+,[\d]+", line)))
         for i, rock in enumerate(matches, 1):
             if i == len(matches):
                 continue
             next_rock = matches[i]
             positions = determine_positions(*rock, *next_rock)
-            if rock[1] == next_rock[1]:
-                air.extend(determine_sub_invalid_rocks(positions))
-                valid_rocks.extend(positions)
-            else:
-                valid_rocks.extend(positions)
-    max_rock_y = determine_max_y_value(valid_rocks)
-    air = list(filter(lambda x: x not in valid_rocks and x[1] <= max_rock_y + 2, air))
-    return list(map(list, set(valid_rocks))), list(map(list, set(air)))
+            valid_rocks.extend(positions)
+    return list(map(list, set(valid_rocks)))
+
 
 def determine_max_y_value(rock_positions):
     return max(map(lambda x: x[1], rock_positions))
 
-def reset_sand_position():
-    return [500, 0]
+
+def pos_to_string(pos):
+    return f"{pos[0]}:{pos[1]}"
 
 
-def determine_sand_units(rock_positions):
+def detemine_next_positions(pos):
+    down = [pos[0], pos[1] + 1]
+    down_left = [pos[0] - 1, pos[1] + 1]
+    down_right = [pos[0] + 1, pos[1] + 1]
+    return down, down_left, down_right
+
+
+def is_valid_move(pos, sand, rocks):
+    return pos_to_string(pos) not in sand and pos_to_string(pos) not in rocks
+
+
+def determine_sand_units(rock_positions, is_infinite):
     max_rock_y = determine_max_y_value(rock_positions)
-    sand_positions = []
-    current_sand_position = reset_sand_position()
+    if is_infinite:
+        max_rock_y += 2
+        rock_positions.extend([[i, max_rock_y] for i in range(1000)])  # Lol
+    rocks = {pos_to_string(pos): 0 for pos in rock_positions}
+    sand_positions = {}
+    current_sand_position = [500, 0]
     while True:
-        if current_sand_position[1] >= max_rock_y:
+        if pos_to_string([500, 0]) in sand_positions or (not is_infinite and current_sand_position[1] == max_rock_y):
             return len(sand_positions)
-        down = [current_sand_position[0], current_sand_position[1] + 1]
-        down_left = [current_sand_position[0] - 1, current_sand_position[1] + 1]
-        down_right = [current_sand_position[0] + 1, current_sand_position[1] + 1]
-
-        if down not in sand_positions and down not in rock_positions:
-            current_sand_position = down
+        down, left, right = detemine_next_positions(current_sand_position)
+        next_position = next(
+            (pos for pos in (down, left, right) if is_valid_move(pos, sand_positions, rocks)), None)
+        if next_position:
+            current_sand_position = next_position
             continue
-        elif down_left not in sand_positions and down_left not in rock_positions:
-            current_sand_position = down_left
-            continue
-        elif down_right not in sand_positions and down_right not in rock_positions:
-            current_sand_position = down_right
-            continue
-        sand_positions.append(current_sand_position)
-        current_sand_position = reset_sand_position()
-
-def determine_max_sand(n):
-    return 2 * (n * (n-1)//2) + n
-
-def determine_max_sand_units(valid_rocks, air):
-    max_rock_y = determine_max_y_value(valid_rocks) + 2
-    max_sand = determine_max_sand(max_rock_y)
-    return max_sand - len(valid_rocks) - len(air)
+        sand_positions[pos_to_string(current_sand_position)] = 0
+        current_sand_position = [500, 0]
 
 
 def main() -> None:
@@ -87,15 +87,13 @@ def main() -> None:
     Main function
     """
     file_input = read_input_file("resources/day14.txt")
-    rocks, air = determine_rocks_and_air(file_input)
+    rocks = determine_rocks(file_input)
 
-    sand_units = determine_sand_units(rocks)
+    sand_units = determine_sand_units(rocks, False)
     print(sand_units)
-
-    part_two = determine_max_sand_units(rocks, air)
+    part_two = determine_sand_units(rocks, True)
     print(part_two)
+
+
 if __name__ == "__main__":
     main()
-
-
-# 23900 too high
